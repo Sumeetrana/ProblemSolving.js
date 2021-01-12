0b7933c991c3fd6a06b7450836ae6c2a88a1f0bd
Sumeetrana <sumeetsinh28@gmail.com>
2021-01-12 20:57:26 +0530

    leetcode/118.Pascal's_Triangle

---
+++ leetcode/118.Pascal's_Triangle.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  var triangle = [];
+  if (numows === 0) {
+    return triangle;
+  }
+  var first_row = [1];
+  triangle.push(first_row);
+  for (var i = 1; i < numRows; i++) {
+    var prev_row = triangle[i - 1];
+    var newRow = [];
+    newRow.push(1);
+    for (var j = 1; j < i; j++) {
+      newRow.push(prev_row[j - 1] + prev_row[j]);
+    }
+    newRow.push(1);
+    triangle.push(newRow);
+  }
+  return triangle;
+};
+
+console.log(generate(6));
